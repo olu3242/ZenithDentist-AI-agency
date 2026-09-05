9d047f490671c079b12e542b9d59cfa173801835
Olufemi Adeyemo <oadeyemo306@gmail.com>
2026-09-04 22:24:57 -0500

    feat(flow-os): expose state and recovery APIs

--- lib/flow-orchestration/index.ts
+++ lib/flow-orchestration/index.ts
@@ -3,4 +3,6 @@ import "@/lib/flow-orchestration/definitions/dental-practice-activation";
 export * from "@/lib/flow-orchestration/types";
 export * from "@/lib/flow-orchestration/registry";
 export * from "@/lib/flow-orchestration/engine";
+export * from "@/lib/flow-orchestration/state";
+export * from "@/lib/flow-orchestration/recovery";
 export { DENTAL_PRACTICE_ACTIVATION_FLOW } from "@/lib/flow-orchestration/definitions/dental-practice-activation";
