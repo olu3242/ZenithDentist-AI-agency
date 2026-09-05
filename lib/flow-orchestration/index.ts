01ab054dc0a0fd3a9807850d577a61bf566c8fed
Olufemi Adeyemo <oadeyemo306@gmail.com>
2026-09-04 22:22:45 -0500

    feat(flow-os): expose canonical orchestration surface

---
+++ lib/flow-orchestration/index.ts
@@ -0,0 +1,6 @@
+import "@/lib/flow-orchestration/definitions/dental-practice-activation";
+
+export * from "@/lib/flow-orchestration/types";
+export * from "@/lib/flow-orchestration/registry";
+export * from "@/lib/flow-orchestration/engine";
+export { DENTAL_PRACTICE_ACTIVATION_FLOW } from "@/lib/flow-orchestration/definitions/dental-practice-activation";
